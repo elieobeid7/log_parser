f6915511853858d05518347324c88736d3131f3e
Elie Obeid <elieobeid7@gmail.com>
2020-02-07 11:54:43 +0200

    initial commit

---
+++ constants.py
@@ -0,0 +1,12 @@
+ENABLE = True
+LOGS_FOLDER_PATH = 'logs/'
+LOG_FILE_BASE_NAME = 'charging_logs-'
+LOG_FILE_EXTENSION = '.log'
+WORD_LIST = ['connect() timed out!', "--couldn't connect to host"]
+INDEX_LOG_NAME = '_index'
+INDEX_LOG_NAME_PATH = 'files/indexes'
+DATE_FORMAT = '%#d.%#m.%#Y'
+FROM_EMAIL = 'example@example.com'
+TO_EMAIL = 'example2@example.com'
+EMAIL_SERVER_HOST = 'smtp.example.com'
+EMAIL_MESSAGE = 'The following errors were found\n'
